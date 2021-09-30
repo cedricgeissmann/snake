e73c39ccf80d01f8b2f88fa0130f2a10da761148
Cedric Geissmann <cedric.geissmann@gmail.com>
2021-09-30 10:35:12 +0200

    Less bugs.

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ import sys
 from random import randint
 
 ## Globals:
-BLOCK_SIZE = 10
-GRID_SIZE = 30
+BLOCK_SIZE = 20
+GRID_SIZE = 60
 FPS = 10
 
 class Registry():
     snakes = []
-    food = None
+    foods = []
 
     @classmethod
     def get_snakes(cls):
@@ -23,39 +23,43 @@ class Registry():
 
     @classmethod
     def add_food(cls, food):
-        cls.food = food
+        cls.foods.append(food)
 
     @classmethod
-    def get_food(cls):
-        if cls.food == None:
-            cls.food = Food()
-        return cls.food
+    def get_foods(cls):
+        return cls.foods
 
 
 class Food():
     pos_x = 0
     pos_y = 0
-    def new(self, surface):
-        pygame.draw.rect(surface, (0, 0, 0), (self.pos_x * BLOCK_SIZE, self.pos_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+    grow = 1
+    def new(self):
         self.pos_x = randint(0, GRID_SIZE - 2)
         self.pos_y = randint(0, GRID_SIZE - 2)
-        pygame.draw.rect(surface, (0, 255, 0), (self.pos_x * BLOCK_SIZE, self.pos_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+        self.grow = randint(1, 3)
 
 
     def get_pos(self):
         return (self.pos_x, self.pos_y)
 
+    def draw(self, surface):
+        color = (0, 255, 0)
+        pygame.draw.rect(surface, color, (self.pos_x * BLOCK_SIZE, self.pos_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+
 class Snake():
-    def __init__(self, ai=True, color=(255, 0, 0)):
-        self.pos_x = randint(1, GRID_SIZE - 2)
+    def __init__(self, ai=True, color=(255, 0, 0), num=1):
+        self.pos_x = randint(1, GRID_SIZE - 10)
         self.pos_y = randint(1, GRID_SIZE - 2)
         self.positions = [self.get_head()]
         self.dir = 0
         self.ai = ai
         self.color = color
+        self.num = num
+        self.growing = 3
 
     def next_dir(self):
-        food = Registry.get_food()
+        food = Registry.get_foods()[0]
         (food_x, food_y) = food.get_pos()
         field = [[abs(x-food_x) + abs(y-food_y) for y in range(GRID_SIZE)] for x in range(GRID_SIZE)]
         for snake in Registry.get_snakes():
@@ -109,12 +113,11 @@ class Snake():
             self.pos_y += 1
 
         self.positions.append(self.get_head())
-        food = Registry.get_food()
-        if self.get_head() != food.get_pos():
+        if self.growing <= 0:
             (x, y) = self.positions.pop(0)
             pygame.draw.rect(surface, (0, 0, 0), (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
         else:
-            food.new(surface)
+            self.growing -= 1
 
         for (x, y) in self.positions:
             pygame.draw.rect(surface, self.color, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
@@ -122,6 +125,27 @@ class Snake():
     def change_dir(self, direction):
         if (self.dir + direction) % 2 == 1:
             self.dir = direction
+
+    def check(self):
+        (head_x, head_y) = self.get_head()
+        if head_x > GRID_SIZE or head_y > GRID_SIZE or head_x < 0 or head_y < 0:
+            player_lost(self.num)
+        for snake in Registry.get_snakes():
+            for (x, y) in snake.positions[:-1]:
+                if (x == head_x and y == head_y):
+                    player_lost(self.num)
+
+        for food in Registry.get_foods():
+            if head_x == food.pos_x and head_y == food.pos_y:
+                self.growing = food.grow
+                food.new()
+
+
+def player_lost(num):
+    print(f"Spieler {num} hat verloren!")
+    pygame.quit()
+    sys.exit()
+
 
 
 def handle_keypress(key):
@@ -138,6 +162,15 @@ def handle_keypress(key):
     elif key == pygame.K_DOWN:
         snakes[0].change_dir(3)
 
+    elif key == pygame.K_d:
+        snakes[1].change_dir(0)
+    elif key == pygame.K_w:
+        snakes[1].change_dir(1)
+    elif key == pygame.K_a:
+        snakes[1].change_dir(2)
+    elif key == pygame.K_s:
+        snakes[1].change_dir(3)
+
 
 def main():
     pygame.init()
@@ -152,12 +185,12 @@ def main():
     clock = pygame.time.Clock()
 
     ## Initialize game objects (snake)
-    snake = Snake(ai=False)
+    snake = Snake(ai=False, num=1)
     Registry.add_snake(snake)
-    Registry.add_snake(Snake(color=(0,0,255)))
+    Registry.add_snake(Snake(ai=True, color=(0,0,255), num=2))
     food = Food()
     Registry.add_food(food)
-    food.new(window)
+    food.new()
 
 
     ## Game loop
@@ -168,9 +201,18 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 handle_keypress(event.key)
+        for x in range(GRID_SIZE):
+            for y in range(GRID_SIZE):
+                color = (0, 0, 0) if (x + y) % 2 == 0 else (32, 32, 32)
+                pygame.draw.rect(window, color, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
 
         for snake in Registry.get_snakes():
             snake.update(window)
+        for food in Registry.get_foods():
+            food.draw(window)
+
+        for snake in Registry.get_snakes():
+            snake.check()
         clock.tick(FPS)
 
 
